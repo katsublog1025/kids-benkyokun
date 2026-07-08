43b551a96207c12fbe3c87a91ebad2f9bacd4239
katsublog1025 <katsu.blog.1025@gmail.com>
2026-07-09 00:33:38 +0900

    .vercelをgitignoreに追加し、ローカル確認用serverを追加

---
+++ server.js
@@ -0,0 +1,28 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = 3001;
+const ROOT = __dirname;
+
+const MIME = {
+  ".html": "text/html; charset=utf-8",
+  ".css": "text/css",
+  ".js": "application/javascript",
+};
+
+http.createServer((req, res) => {
+  let filePath = path.join(ROOT, req.url === "/" ? "index.html" : req.url);
+  const ext = path.extname(filePath);
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end("Not found");
+      return;
+    }
+    res.writeHead(200, { "Content-Type": MIME[ext] || "text/plain" });
+    res.end(data);
+  });
+}).listen(PORT, () => {
+  console.log(`Kids Quiz server running at http://localhost:${PORT}`);
+});
